ec60f2f97489121ea9edef88bdf3b45011ddf986
李岚 <18834078298@163.com>
2018-11-07 16:14:01 +0800

    用户名和手机号接口的实现

--- front/js/register.js
+++ front/js/register.js
@@ -15,7 +15,9 @@ var vm = new Vue({
 		mobile: '', 
 		image_code: '',
 		sms_code: '',
-		allow: false
+		allow: false,
+		error_name_message: '请输入5-20个字符的用户',
+		error_phone_message: '您输入的手机号格式不正确',
 	},
 	methods: {
 		check_username: function (){
@@ -25,6 +27,7 @@ var vm = new Vue({
 			} else {
 				this.error_name = false;
 			}
+
 		},
 		check_pwd: function (){
 			var len = this.password.length;
@@ -32,8 +35,25 @@ var vm = new Vue({
 				this.error_password = true;
 			} else {
 				this.error_password = false;
-			}		
-		},
+			}
+			            // 检查重名
+            if (this.error_name == false) {
+                axios.get('http://127.0.0.1:8000'+'/users/usernames/' + this.username + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        } else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
+        },
 		check_cpwd: function (){
 			if(this.password!=this.password2) {
 				this.error_check_password = true;
@@ -41,14 +61,31 @@ var vm = new Vue({
 				this.error_check_password = false;
 			}		
 		},
-		check_phone: function (){
-			var re = /^1[345789]\d{9}$/;
-			if(re.test(this.mobile)) {
-				this.error_phone = false;
-			} else {
-				this.error_phone = true;
-			}
-		},
+	check_phone: function (){
+				var re = /^1[345789]\d{9}$/;
+				if(re.test(this.mobile)) {
+					this.error_phone = false;
+				} else {
+					this.error_phone_message = '您输入的手机号格式不正确';
+					this.error_phone = true;
+				}
+				if (this.error_phone == false) {
+					axios.get('http://127.0.0.1:8000'+'/users/phones/'+ this.mobile + '/count/', {
+							responseType: 'json'
+						})
+						.then(response => {
+							if (response.data.count > 0) {
+								this.error_phone_message = '手机号已存在';
+								this.error_phone = true;
+							} else {
+								this.error_phone = false;
+							}
+						})
+						.catch(error => {
+							console.log(error.response.data);
+						})
+				}
+			},
 		check_image_code: function (){
 			if(!this.image_code) {
 				this.error_image_code = true;
